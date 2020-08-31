322fad98d889c0ad9b798ed111f187ccbf938537
Emrah ESEN <emrah-esen@hotmail.com>
2020-08-31 19:20:27 +0300

    pageview eklendi.

---
+++ lib/ui/shopping_list_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ShoppingListPage extends StatefulWidget {
+  @override
+  _ShoppingListPageState createState() => _ShoppingListPageState();
+}
+
+class _ShoppingListPageState extends State<ShoppingListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Shopping List"),),
+      body: PageView(
+        children: [
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.blue,
+          ),
+          Container(
+            color: Colors.orange,
+          )
+        ],
+      ),
+    );
+  }
+}
